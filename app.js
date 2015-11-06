b92166722a59d4f2f0fdca7bcb35f0a63f8a7735
Joel Elizaga <joelizaga@hotmail.com>
2015-11-05 17:59:14 -0800

    Initial commit. Pretty unchanged.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-var myApp = angular.module('myApp', [])
+var baseUrl = "https://api.spotify.com/v1/search?type=track&query=";
+var myApp = angular.module("myApp", []);
 
-var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+
+var myCtrl = myApp.controller("myCtrl", function($scope, $http) {
+
   $scope.audioObject = {}
+
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
       
     })
   }
+
   $scope.play = function(song) {
-    if($scope.currentSong == song) {
+    if ($scope.currentSong == song) {
       $scope.audioObject.pause()
       $scope.currentSong = false
       return
@@ -19,13 +23,14 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     else {
       if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
       $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
-      $scope.currentSong = song
+      $scope.audioObject.play();
+      $scope.currentSong = song;
     }
   }
+
 })
 
 // Add tool tips to anything with a title property
-$('body').tooltip({
-    selector: '[title]'
+$("body").tooltip({
+    selector: "[title]"
 });
