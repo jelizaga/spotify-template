f6b0c0bc3f9c9d7d4d0bdbd7206f4e3aeb5f99ff
Joel Elizaga <joelizaga@hotmail.com>
2015-11-06 16:01:53 -0800

    More css, fixing js.

--- app.js
+++ app.js
@@ -4,41 +4,29 @@ var baseUrl = "https://api.spotify.com/v1/search?type=track&query=";
 // Initializing angular app; joelsSpotify.
 var joelsSpotify = angular.module("joelsSpotify", []);
 
-// joelsSpotifyCtrl
 // $scope is literally the scope of the angular.js application.
 // $http is the variable that contains the AJAX service of angular.js.
-var myCtrl = joelsSpotify.controller("myCtrl", function($scope, $http) {
-
-  $scope.audioObject = {};
-
+var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+  $scope.audioObject = {}
   $scope.getSongs = function() {
-
     $http.get(baseUrl + $scope.track).success(function(response){
-
       data = $scope.tracks = response.tracks.items
       
     })
   }
-
-  // 
   $scope.play = function(song) {
-
-    if ($scope.currentSong == song) {
-
-      $scope.audioObject.pause();
-      $scope.currentSong = false;
-      return;
-
-    } else {
-      if ($scope.audioObject.pause != undefined) $scope.audioObject.pause()
-        $scope.audioObject = new Audio(song);
-        $scope.audioObject.play()
-        $scope.currentSong = song
-
-      }
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause()
+      $scope.currentSong = false
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play()  
+      $scope.currentSong = song
     }
   }
-
 })
 
 // Add tool tips to anything with a title property
